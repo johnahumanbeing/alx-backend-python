3dbb665c17514de98861bd6f1acbd86169f46d64
johnahumanbeing <mseewak@gmail.com>
2024-05-25 20:04:50 +0300

    Mock HTTP calls and parametizing patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -54,3 +54,33 @@ class TestGetJson(unittest.TestCase):
         with patch('requests.get') as mock_get:
             mock_get.return_value.json.return_value = test_payload
             self.assertEqual(get_json(test_url), test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """ Test memoize function
+    """
+
+    def test_memoize(self) -> None:
+        """ Test memoize function
+        """
+
+        class TestClass:
+            """ TestClass
+            """
+
+            def a_method(self) -> int:
+                """ a_method
+                """
+                return 42
+
+            @memoize
+            def a_property(self) -> int:
+                """ a_property
+                """
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=42) as mocked:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property, 42)
+            self.assertEqual(test_class.a_property, 42)
+            mocked.assert_called_once()
